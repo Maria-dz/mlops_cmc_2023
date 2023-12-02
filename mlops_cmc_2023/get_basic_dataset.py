9d085019e0d616eca33ec478be48b9b9a41ea7c9
Maria Dzyuba <mary.boom2001@gmail.com>
2023-12-03 00:02:00 +0300

    adding data to google drive through dvc

--- mlops_cmc_2023/get_basic_dataset.py
+++ mlops_cmc_2023/get_basic_dataset.py
@@ -9,3 +9,12 @@ def get_train_data():
 def get_test_data():
     X, y = fetch_openml("mnist_784", return_X_y=True, parser="auto")
     return X.iloc[6000:], y[6000:]
+
+
+if __name__ == "__main__":
+    train_df = get_train_data()[0]
+    train_df["target"] = get_train_data()[1]
+    train_df.to_csv("train_data.csv")
+    test_df = get_test_data()[0]
+    test_df["target"] = get_test_data()[1]
+    test_df.to_csv("test_data.csv")
