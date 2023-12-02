b5e5dac4ae5845a36052e1dc7204820ad323ac55
Maria Dzyuba <mary.boom2001@gmail.com>
2023-12-03 01:42:22 +0300

    changing train and infer to read from remote storage

--- mlops_cmc_2023/train.py
+++ mlops_cmc_2023/train.py
@@ -1,10 +1,17 @@
+import io
 import pickle
 
-import get_basic_dataset as data
+import dvc.api
 import knn_classifier as knn_tools
+import pandas as pd
 
 if __name__ == "__main__":
-    X, y = data.get_train_data()
+    smth = dvc.api.read("../data/test_data.csv")
+    train_string = io.StringIO(smth)
+    df = pd.read_csv(train_string, sep=",")
+
+    y = df["target"]
+    X = df.drop("target", axis=1)
     model = knn_tools.KNNClassifier(k=500, strategy="auto", metric="cosine")
     model = model.fit(X, y)
     with open("model_trained.pkl", "wb") as f:
